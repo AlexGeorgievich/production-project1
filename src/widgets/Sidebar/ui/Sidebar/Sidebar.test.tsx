09f2679df89e336a48b8b28f12f448b8211c4cd6
AlexGeorgievich <alexhome1@yandex.ru>
2023-02-20 18:47:09 +0300

    fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.test.tsx
@@ -2,18 +2,19 @@
 import { fireEvent, screen } from '@testing-library/react';
 import { Sidebar } from 'widgets/Sidebar';
 import { renderWithTranslation } from 'shared/lib/tests/renderWitnTrnslation/renderWithTranslation';
+import {componentRender} from "shared/lib/tests/componentRender/componentRender";
 
 describe('Sidebar', () => {
     test('Sidebar', () => {
         // const SidebarWithTranslation = withTranslation()(Sidebar);
         // render(<SidebarWithTranslation />);
-        renderWithTranslation(<Sidebar />);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
 
     // тест на разворачивание и сворачивание Sidebar
     test('test toggle', () => {
-        renderWithTranslation(<Sidebar />); // render screen
+        componentRender(<Sidebar />); // render screen
         const toggleBtn = screen.getByTestId('sidebar-toggle'); // получили кнопку по идентификатору 'sidebar-toggle'
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn); // press on the Button
